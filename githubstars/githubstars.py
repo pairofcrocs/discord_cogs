1d78c6fb140c26fb47495afe0b6d667cc954a086
crocs <34723037+pairofcrocs@users.noreply.github.com>
2022-12-15 21:45:20 -0600

    Update githubstars.py

--- githubstars/githubstars.py
+++ githubstars/githubstars.py
@@ -1,31 +1,32 @@
 import discord
 import aiohttp
-import redbot
+import asyncio
 from discord.ext import commands
 from discord.utils import get
-from redbot.core import tasks
 
 class GithubStarsCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.session = aiohttp.ClientSession()
+        self.bot.loop.create_task(self.update_github_stars_loop(channel, repository))  # start the looping task
 
-async def create_github_stars_channel(self, ctx):
-    category = get(ctx.guild.categories, name="Server Stats")
-    overwrites = {
-        ctx.guild.default_role: discord.PermissionOverwrite(connect=False)
-    }
-    return await ctx.guild.create_voice_channel("Github Stars", category=category, overwrites=overwrites)
+    async def create_github_stars_channel(self, ctx):
+        category = get(ctx.guild.categories, name="Server Stats")
+        overwrites = {
+            ctx.guild.default_role: discord.PermissionOverwrite(connect=False)
+        }
+        return await ctx.guild.create_voice_channel("Github Stars", category=category, overwrites=overwrites)
 
-async def update_github_stars(self, channel, repository):
-    async with self.session.get(f"https://api.github.com/repos/{repository}") as resp:
-        data = await resp.json()
-        stars = data["stargazers_count"]
-        await channel.edit(name=f"Github Stars: {stars}")
+    async def update_github_stars(self, channel, repository):
+        async with self.session.get(f"https://api.github.com/repos/tubearchivist/tubearchivist") as resp:
+            data = await resp.json()
+            stars = data["stargazers_count"]
+            await channel.edit(name=f"Github Stars: {stars}")
 
-    @tasks.loop(hours=1)
     async def update_github_stars_loop(self, channel, repository):
-        await self.update_github_stars(channel, repository)
+        while True:
+            await self.update_github_stars(channel, repository)
+            await asyncio.sleep(3600)  # pause for 1 hour
 
 def setup(bot):
     bot.add_cog(GithubStarsCog(bot))
